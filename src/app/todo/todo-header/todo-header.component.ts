bdf9955d3837375a8204617f4953d2821a181c00
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-08-03 01:13:33 +0200

    Feature: Add todo header input with service addTodo

--- src/app/todo/todo-header/todo-header.component.ts
+++ src/app/todo/todo-header/todo-header.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {TodoService} from "../todo.service";
+import {MatInputModule} from "@angular/material/input";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatCheckbox} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-todo-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCheckbox
+  ],
   templateUrl: './todo-header.component.html',
   styleUrl: './todo-header.component.scss'
 })
 export class TodoHeaderComponent {
+  newTodoTitle: string = '';
+  completed: boolean = false;
 
+  constructor(private todoService: TodoService) {
+  }
+
+  addTodo(): void {
+    if (this.newTodoTitle.trim().length) {
+      this.todoService.addTodo(this.newTodoTitle, this.completed);
+      this.newTodoTitle = '';
+      this.completed = false
+    }
+  }
+
+  toggleCheck(): void {
+    this.completed = !this.completed;
+  }
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {TodoModel} from '../todo/todo.model';
+import {v4 as uuidv4} from 'uuid';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TodosService {
+export class TodoService {
+  private todos: TodoModel[] = [];
+  
+  addTodo(title: string, completed: boolean): void {
+    const newTodo: TodoModel = new TodoModel(uuidv4(), title, completed);
+    this.todos.push(newTodo);
+  }
 
-  constructor() { }
 }
