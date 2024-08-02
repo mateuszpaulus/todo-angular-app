b32680db6fbb94dc3318da61738d179b4f19ea74
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-08-03 01:15:14 +0200

    Feature: Add main todo view with imported components

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {TodoFooterComponent} from "./todo-footer/todo-footer.component";
+import {TodoHeaderComponent} from "./todo-header/todo-header.component";
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [
+    TodoListComponent,
+    TodoFooterComponent,
+    TodoHeaderComponent,
+  ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
