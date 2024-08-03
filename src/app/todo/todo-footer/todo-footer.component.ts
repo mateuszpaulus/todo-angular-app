bae8a0a338855fa65941d49b3dcf4fe94cbf73cb
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-08-03 13:15:26 +0200

    Feature: Add todo footer with service to manage todos

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TodoService} from "../todo.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
   templateUrl: './todo-footer.component.html',
   styleUrl: './todo-footer.component.scss'
 })
-export class TodoFooterComponent {
 
+export class TodoFooterComponent implements OnInit, OnDestroy {
+  activeCount: number = 0;
+  todoSub: Subscription = new Subscription();
+  filterSub: Subscription = new Subscription();
+  filter: string = '';
+
+
+  constructor(private todoService: TodoService) {
+  }
+
+  ngOnInit(): void {
+    this.todoSub = this.todoService.getTodos().subscribe(todos => {
+      this.activeCount = todos.filter(todo => !todo.completed).length;
+    });
+    this.filterSub = this.todoService.filter$.subscribe(filter => {
+      this.filter = filter;
+    });
+  }
+
+  clearCompleted(): void {
+    this.todoService.clearCompletedTodos();
+  }
+
+  setFilter(filter: string): void {
+    this.todoService.setFilter(filter);
+  }
+
+  ngOnDestroy(): void {
+    this.todoSub.unsubscribe();
+    this.filterSub.unsubscribe();
+  }
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,16 +1,71 @@
 import {Injectable} from '@angular/core';
 import {TodoModel} from '../todo/todo.model';
 import {v4 as uuidv4} from 'uuid';
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  private todos: TodoModel[] = [];
-  
+  private todosSubject = new BehaviorSubject<TodoModel[]>([]);
+  todos$: Observable<TodoModel[]> = this.todosSubject.asObservable();
+  private filterSubject = new BehaviorSubject<string>('all');
+  filter$: Observable<string> = this.filterSubject.asObservable();
+
+  private todos: TodoModel[] = [{id: '1', title: 'sss11', completed: false}, {id: '2', title: 'sss', completed: true}];
+
+  constructor() {
+    this.todosSubject.next(this.todos);
+  }
+
+  getTodos(): Observable<TodoModel[]> {
+    return this.todos$;
+  }
+
   addTodo(title: string, completed: boolean): void {
     const newTodo: TodoModel = new TodoModel(uuidv4(), title, completed);
     this.todos.push(newTodo);
+    this.todosSubject.next(this.todos);
+  }
+
+  deleteTodo(id: string): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+    this.todosSubject.next(this.todos);
+  }
+
+  toggleTodoCompletion(id: string): void {
+    const todo = this.todos.find(todo => todo.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+    }
+  }
+
+  clearCompletedTodos(): void {
+    this.todos = this.todos.filter(todo => !todo.completed);
+    this.todosSubject.next(this.todos);
+  }
+
+  setFilter(filter: string): void {
+    this.filterSubject.next(filter);
+    this.updateTodos();
+  }
+
+  private updateTodos(): void {
+    const filter = this.filterSubject.getValue();
+    let filteredTodos: TodoModel[];
+
+    switch (filter) {
+      case 'active':
+        filteredTodos = this.todos.filter(todo => !todo.completed);
+        break;
+      case 'completed':
+        filteredTodos = this.todos.filter(todo => todo.completed);
+        break;
+      default:
+        filteredTodos = this.todos;
+    }
+
+    this.todosSubject.next(filteredTodos);
   }
 
 }
